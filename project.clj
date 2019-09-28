9ed57e4e4f3e731f0f80c21460aee9f30a2573c3
Rothanak So <rothanak.hello@gmail.com>
2019-09-28 14:10:02 -0400

    Implement an almost-working A* algorithm!

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/math.combinatorics "0.1.6"]]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [org.clojure/data.priority-map "0.0.10"]]
   :repl-options {:init-ns pathfinder.core})
--- src/pathfinder/core.clj
+++ src/pathfinder/core.clj
@@ -1,11 +1,16 @@
 (ns pathfinder.core
   (:require [clojure.math.numeric-tower :as math])
-  (:require [clojure.set :as set]))
+  (:require [clojure.set :as set])
+  (:require [clojure.data.priority-map :refer [priority-map]]))
 
 (defn rotate [n a]
   (let [l (count a)
         off (mod (+ (mod n l) l) l)]
     (concat (drop off a) (take off a))))
+
+(defn update-map [f m]
+  (reduce-kv (fn [m k v]
+               (assoc m k (f k v))) {} m))
 
 (def shapes
   {:a [[2 6] [17 6] [17 1] [2 1]]
@@ -26,8 +31,8 @@
 (defn distance [vert-a vert-b]
   (math/sqrt (reduce + (map #(math/expt % 2) (map - vert-a vert-b)))))
 
-(defn create-edge [[_ vert-a :as a] [_ vert-b :as b]]
-  {:from a :to b :dist (distance vert-a vert-b)})
+(defn create-edge [a b]
+  {:from a :to b})
 
 (defn flip [edge]
   (set/rename-keys edge {:from :to, :to :from}))
@@ -47,8 +52,11 @@
 (defn obstacle-edges []
   (set (flatten (map neighboring-edges (shape-verts)))))
 
+(defn search-space []
+  (conj (apply concat (shape-verts)) goal))
+
 (defn potential-edges [vertex]
-  (map #(create-edge vertex %) (apply concat (shape-verts))))
+  (map #(create-edge vertex %) (search-space)))
 
 (defn visible? [{[_ a] :from [_ b] :to}]
   (not-any? (fn [{[_ c] :from [_ d] :to}] (intersecting? a b c d)) (obstacle-edges)))
@@ -61,4 +69,32 @@
 (defn get-visible-vertices [vertex]
   (->> (potential-edges vertex)
        (filter valid-edge?)
-       (filter visible?)))
+       (filter visible?)
+       (map #(:to %))))
+
+(defn h [n]
+  (distance (second n) (second goal)))
+
+(defn A* []
+  (loop [open-list (priority-map start (h start))
+         closed-list #{}
+         came-from []
+         g-score (assoc (zipmap (search-space) (repeat ##Inf)) start 0)]
+    (when (not (empty? open-list))
+      (let [g (fn [from to] (+ (get g-score from) (distance (second from) (second to))))
+            current (key (peek open-list))
+            neighbors (filter #(not (contains? closed-list (:to %))) (get-visible-vertices current))
+            g-score' (->> neighbors
+                          (map #(g current %))
+                          (zipmap neighbors)
+                          (filter #(< (val %) (g-score (key %))))
+                          (into {}))
+            f-score' (update-map #(+ %2 (h %1)) g-score')]
+        (println current)
+        (if (= goal current)
+          "hello"                                           ;; todo
+          (recur
+            (into (pop open-list) f-score')                 ;; add improved neighbors to open list
+            (conj closed-list current)                      ;; add current vertex to the closed list
+            came-from
+            (merge-with min g-score g-score')))))))         ;; update the g-scores with the minimum
